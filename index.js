e60fda0438e9c1c6e0127fc5d4832e181d9b42fb
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-01-18 12:22:01 +0000

    Add default get route response

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ import { expressCfg } from "./config.js";
 
 const app = express();
 
-const PORT = 3010 || expressCfg.PORT;
+const PORT = expressCfg.PORT || 3010;
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    payload: "This is the default Express API route",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Express server is listening at http://localhost:${PORT}`);
